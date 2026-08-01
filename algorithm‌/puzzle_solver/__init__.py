d3ce30f5c677beb366eda25c9a2b76c358c232c8
123 <123@123.com>
2026-08-01 11:48:35 +0000

    team2:init

--- algorithm‌/puzzle_solver/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Geometry solver for the E-problem puzzle device."""
-
-from .solver import SolverConfig, solve_puzzle
-from .coordinates import A4ToGrblTransform
-
-__all__ = ["SolverConfig", "solve_puzzle", "A4ToGrblTransform"]
